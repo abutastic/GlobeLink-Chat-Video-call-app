00d41208f5873094ccfd8ff610df3f99021032d8
Abbas W <abbas.code786@gmail.com>
2025-05-15 18:36:59 +0530

    fetching mechanism added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,8 +8,22 @@ import CallPage from "./pages/CallPage.jsx";
 import NotificationsPage from "./pages/NotificationsPage.jsx";
 import OnboardingPage from "./pages/OnboardingPage.jsx";
 import { Toaster } from "react-hot-toast";
+import { useQuery } from "@tanstack/react-query";
+import { axiosInstance } from "./lib/axios.js";
 
 function App() {
+  //tanstack query
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["todos"],
+    queryFn: async () => {
+      const res = await axiosInstance.get("/auth/me");
+
+      return res.data;
+    },
+    retry: false,
+  });
+
+  console.log(data);
   return (
     <div className="h-screen" data-theme="night">
       <Routes>

21f6c84e07a39a5d645e3ba26b3ff3e1efa51b8b
Abbas W <abbas.code786@gmail.com>
2025-07-14 18:48:38 +0530

    code optimisation

--- backend/src/server.js
+++ backend/src/server.js
@@ -16,7 +16,7 @@ const __dirname = path.resolve();
 app.use(
   cors({
     origin: "http://localhost:5173",
-    credentials: true, // allow frontend to
+    credentials: true, // allow frontend to send
   })
 );
 

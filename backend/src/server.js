656f12919c8c708fc363d1ed88855346d89b543a
Abbas W <abbas.code786@gmail.com>
2025-07-16 17:13:58 +0530

    getUsers route updated

--- backend/src/server.js
+++ backend/src/server.js
@@ -16,7 +16,7 @@ const __dirname = path.resolve();
 app.use(
   cors({
     origin: "http://localhost:5173",
-    credentials: true, // allow   to send cookies
+    credentials: true, // allow frontend to send cookies
   })
 );
 

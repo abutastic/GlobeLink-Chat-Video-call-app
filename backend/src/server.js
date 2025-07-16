49f6b4b82faee6a6ac65059c4287c0cf6cdffb07
Abbas W <abbas.code786@gmail.com>
2025-07-16 17:12:59 +0530

    bug fixes

--- backend/src/server.js
+++ backend/src/server.js
@@ -16,7 +16,7 @@ const __dirname = path.resolve();
 app.use(
   cors({
     origin: "http://localhost:5173",
-    credentials: true, //  frontend to send cookies
+    credentials: true, //   to send cookies
   })
 );
 
